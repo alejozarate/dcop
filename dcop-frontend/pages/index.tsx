c83a1ececb1b9112682eff8f8f4e9dd5b165b73c
Alejo Zarate <Alejo@192.168.0.51>
2022-10-09 17:48:34 -0500

    [Prepare] deploy

--- dcop-frontend/pages/index.tsx
+++ dcop-frontend/pages/index.tsx
@@ -2,15 +2,17 @@ import type { NextPage } from 'next'
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { usePrepareContractWrite, useContractWrite, useAccount, useNetwork } from 'wagmi'
 import dCOPABI from '../constants/ABI.json'
-
+import Image from 'next/image'
 import "@rainbow-me/rainbowkit/styles.css";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
 const dCOPAddress = '0x1a2BcD22BAdd4AcF274A15a81B2a23Fc53232B32';
 
 const Home: NextPage = () => {
 
+  const [amountToMint, setAmountToMint] = useState(0);
+  const [amountToDeposit, setAmountToDeposit] = useState(0);
 
   const { config: createVaultConfig } = usePrepareContractWrite({
     addressOrName: dCOPAddress,
@@ -34,7 +36,7 @@ const Home: NextPage = () => {
     functionName: 'depositCollateral',
     args: ["0"],
     overrides: {
-      value: ethers.utils.parseEther('0.1')
+      value: ethers.utils.parseEther(amountToDeposit.toString())
     }
   })
 
@@ -44,7 +46,7 @@ const Home: NextPage = () => {
     addressOrName: dCOPAddress,
     contractInterface: dCOPABI,
     functionName: 'borrowToken',
-    args: ["0", ethers.utils.parseEther('0.002').toString()],
+    args: ["0", ethers.utils.parseEther(amountToMint.toString()).toString()],
     overrides: {
       gasLimit: 2000000
     }
@@ -69,12 +71,25 @@ const Home: NextPage = () => {
 
     write()
   }
+
   return (
     <div>
       <ConnectButton />
-      <div style={{cursor: 'pointer'}} onClick={() => createVault()}>Create vault</div>
-      <div onClick={() => depositCollateral()}>Deposit collateral</div>
-      <div onClick={() => borrowTokens()}>Mint dCOP</div>
+      <div className="flex flex-col gap-2 items-center justify-center">
+        <Image src={'/dcop.png'} height={200} width={200} alt="dCOP"/>
+        <div className='flex gap-3'>
+          <div className='flex justify-center items-center'>
+            <div>
+          <div className="p-2 bg-yellow-300 cursor-pointer" onClick={() => createVault()}>Create vault</div></div>
+          </div>
+          <div className='flex flex-col gap-2 border rounded-md'><input value={amountToDeposit} onChange={(e) => setAmountToDeposit(Number(e.target.value))}/><div className="p-2 bg-yellow-300 cursor-pointer" onClick={() => depositCollateral()}>Deposit collateral</div></div>
+          
+          <div className='flex flex-col gap-2 border rounded-md'>
+            <input value={amountToMint} onChange={(e) => setAmountToMint(Number(e.target.value))}/>
+          <div className="p-2 bg-yellow-300 cursor-pointer" onClick={() => borrowTokens()}>Mint dCOP</div>
+          </div>
+      </div>
+      </div>
     </div>
   )
 }
